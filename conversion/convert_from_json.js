a78c6f391c7011c11db91e8e7161e412cca57047
Lars van Kleij <larsvankleij@gmail.com>
2025-02-06 14:18:58 +0100

    Add convert_from_json.js

---
+++ conversion/convert_from_json.js
@@ -0,0 +1,28 @@
+// Converter to convert exported form custom_form.json to formio form definition.
+const fs = require('fs');
+const path = require('path');
+
+const formJson = fs.readFileSync(path.join(__dirname, 'custom_form.json'), 'utf8');
+const form = JSON.parse(formJson);
+
+const components = form.definition.formioFormDefinition.components;
+const customCss = form.definition.customCss;
+const customJs = form.definition.customJs;
+
+// Write the form components to form/public/form.js
+const formJsContent = `Formio.createForm(document.getElementById('dvf-form-viewer'), {\n    components: ${JSON.stringify(components, null, 4)}\n});`;
+fs.writeFileSync(path.join(__dirname, '..', 'form', 'public', 'js', 'form.js'), formJsContent);
+
+// Write the custom css to form/public/customcss.css
+fs.writeFileSync(path.join(__dirname, '..', 'form', 'public', 'customcss.css'), customCss);
+
+// Write the custom js to form/public/js/main.js
+fs.writeFileSync(path.join(__dirname, '..', 'form', 'public', 'js', 'main.js'), customJs);
+
+// Get all function names from form/public/js/main.js
+const mainJsContent = fs.readFileSync(path.join(__dirname, '..', 'form', 'public', 'js', 'main.js'), 'utf8');
+const functionNames = mainJsContent.match(/function\s+(\w+)\s*\(/g).map(match => match.match(/function\s+(\w+)\s*\(/)[1]);
+
+// Write the content of main.js to form/public/js/main.js and export the function names
+const mainJsExportContent = `${mainJsContent}\n\nexport {\n    ${functionNames.join(',\n    ')}\n};\n`;
+fs.writeFileSync(path.join(__dirname, '..', 'form', 'public', 'js', 'main.js'), mainJsExportContent);
---
+++ convert_from_json.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set /p response=Convert custom_form.json to local Formio form? This will overwrite the files in the 'form/' directory. (Y/n)
+
+:: If n or N or no or NO is entered, exit the script
+if /i "%response%"=="n" goto no
+if /i "%response%"=="N" goto no
+if /i "%response%"=="no" goto no
+if /i "%response%"=="NO" goto no
+if /i "%response%"=="No" goto no
+if /i "%response%"=="nO" goto no
+goto yes
+
+:no
+echo Operation cancelled.
+goto end
+
+:yes
+echo Converting form to JSON format...
+
+node conversion/convert_from_json.js
+
+echo Conversion complete. Files in the 'form/' directory have been overwritten.
+
+:: Wait for 5 seconds
+timeout /t 5
+
+:end
